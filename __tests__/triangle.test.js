d33761ac20f495a7f36eac4abffaf11a4e860fe3
Giancarlo Vigneri <bobloblaw.vigneri@gmail.com>
2021-04-18 22:54:27 -0700

    removed parens from checkType in expect lines

--- __tests__/triangle.test.js
+++ __tests__/triangle.test.js
@@ -11,21 +11,21 @@ describe('Triangle', () => {
 
   test('should correctly determine whether three lengths are not a triangle', () => {
       const notTriangle = new Triangle(3,9,22);
-      expect(notTriangle.checkType()).toEqual("not a triangle");
+      expect(notTriangle.checkType).toEqual("not a triangle");
     });
 
   test('should correctly determine whether three lenghts make a scalene triangle', () => {
       const scalTriangle = new Triangle(4,5,7);
-      expect(scalTriangle.checkType()).toEqual("scalene triangle");
+      expect(scalTriangle.checkType).toEqual("scalene triangle");
     });
 
   test('should correctly determine whether three lengths make an isosceles triangle', () => {
       const isocTriangle = new Triangle(5,5,7);
-      expect(isocTriangle.checkType()).toEqual("isosceles triangle");
+      expect(isocTriangle.checkType).toEqual("isosceles triangle");
     });
 
   test('should correctly determine whether three lengths make an equilateral triangle', () => {
       const equilTriangle = new Triangle(5,5,5);
-      expect(equilTriangle.checkType()).toEqual("equilateral triangle");
+      expect(equilTriangle.checkType).toEqual("equilateral triangle");
     });
 });
